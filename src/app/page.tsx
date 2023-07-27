732e85aed42bf4432b26b2d7e0764e58648ce30e
Afolabi Samuel Adedeji <97afolabi@gmail.com>
2023-07-27 17:17:15 +0100

    feat: implement user authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 "use client";
+import axios from "axios";
+import { SetStateAction, useState } from "react";
 import styled from "styled-components";
+import { setCookie } from "cookies-next";
+import { ApiRoutes } from "../routes";
 
 const MainContainer = styled.main`
   display: flex;
@@ -51,16 +55,88 @@ const CenterSection = styled.section`
 `;
 
 const HomePage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const [errors, setErrors] = useState([]);
+
+  const handleUsernameChange = (e: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(ApiRoutes.login, {
+        username,
+        password,
+      });
+
+      localStorage.setItem("username", username);
+
+      const currentTime = new Date();
+      // Add 1 hour to the current time
+      const expires = new Date(
+        currentTime.setHours(currentTime.getHours() + 1)
+      );
+
+      setCookie("token", response.data.token, { expires });
+      setMessage(response.data.message);
+      setErrors([]);
+    } catch (error) {
+      if (error.response && error.response.data && error.response.data.errors) {
+        setErrors(error.response.data.errors);
+      } else {
+        setMessage(error.response.data.message);
+      }
+    }
+  };
+
   return (
     <MainContainer>
       <CenterSection>
         <p>Hi there! Welcome to your education showcase</p>
-        <form>
+
+        <form onSubmit={handleSubmit}>
+          {message && <p>{message}</p>}
+          {errors.length > 0 && (
+            <p>
+              {errors.map((error, index) => (
+                <small
+                  key={index}
+                  style={{ display: "block", color: "#f44336" }}
+                >
+                  {error.message}
+                </small>
+              ))}
+            </p>
+          )}
           <p>Type your name and click &lsquo;Enter&rsquo; below to begin!</p>
           <label htmlFor="username">Username</label>
-          <input type="text" id="username" />
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={handleUsernameChange}
+            required
+          />
           <label htmlFor="password">Password</label>
-          <input type="password" id="password" />
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
           <input type="submit" value="Enter" />
         </form>
       </CenterSection>
